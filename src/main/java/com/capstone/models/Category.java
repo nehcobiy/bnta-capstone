7ddbf6a5c8f37d6cf3207b10b5b5030eef052c88
hadams0 <adamshannah1@icloud.com>
2023-09-22 10:22:12 +0100

    finished the models

--- src/main/java/com/capstone/models/Category.java
+++ src/main/java/com/capstone/models/Category.java
@@ -1,4 +1,21 @@
 package com.capstone.models;
 
 public enum Category {
+
+    BOOKS("Books"),
+    HOMEWARE("Homeware"),
+    ELECTRONICS("Electronics"),
+    TOYS("Toys"),
+    MEDIA("Media");
+
+    private final String displayCategoryName;
+
+    Category(String displayCategoryName){
+        this.displayCategoryName = displayCategoryName;
+    }
+
+    public String getDisplayCategoryName(){
+        return displayCategoryName;
+    }
+
 }
--- src/main/java/com/capstone/models/OrderProduct.java
+++ src/main/java/com/capstone/models/OrderProduct.java
@@ -1,4 +1,66 @@
 package com.capstone.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders_products")
 public class OrderProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @JsonIgnoreProperties({"orders_products"})
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnoreProperties({"orders_products"})
+    private Product product;
+
+    @Column
+    private int quantity;
+
+    public OrderProduct() {}
+
+    public OrderProduct(Order order, Product product, int quantity) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
