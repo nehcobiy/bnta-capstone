08a5666e53a9edfc4eafb21ba45c8cb1a326df43
darkpoetOX <ayindejam2009@yahoo.com>
2023-09-22 11:38:02 +0100

    populated data loader

--- src/main/java/com/capstone/controllers/OrderController.java
+++ src/main/java/com/capstone/controllers/OrderController.java
@@ -1,4 +1,9 @@
 package com.capstone.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("orders")
 public class OrderController {
 }
