68fd361bf3369bd9857606f3d442396e69b4f1db
darkpoetOX <ayindejam2009@yahoo.com>
2023-09-22 14:47:45 +0100

    made customer service and ammended customer controller

--- src/main/java/com/capstone/services/CustomerService.java
+++ src/main/java/com/capstone/services/CustomerService.java
@@ -1,4 +1,32 @@
 package com.capstone.services;
 
+import com.capstone.models.Customer;
+import com.capstone.repositories.CustomerRepository;
+import com.capstone.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Customer> findAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> findCustomerById(Long id){
+        return customerRepository.findById(id);
+    }
+
+    public Customer saveCustomer(Customer customer){
+       return customerRepository.save(customer);
+    }
 }
