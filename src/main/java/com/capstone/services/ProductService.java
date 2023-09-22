b56de34225156896d4428a4c93f78dad553ba89a
nehcobiy <yibo-chen@hotmail.com>
2023-09-22 12:24:44 +0100

    finished get product by id

--- src/main/java/com/capstone/services/ProductService.java
+++ src/main/java/com/capstone/services/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -15,6 +16,10 @@ public class ProductService {
 
     public List<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Optional<Product> findProductById(Long id){
+        return productRepository.findById(id);
     }
 
 
